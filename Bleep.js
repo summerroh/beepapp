4785f1a0b333b51f56718b7ffd79c72dd5709281
summerroh <summerrohh@gmail.com>
2021-08-11 19:32:07 +0900

    sound button works, now need to make that we ca hear the actual sound

--- Bleep.js
+++ Bleep.js
@@ -1,65 +1,92 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, StatusBar, Image, ImageBackground, TouchableWithoutFeedback , Alert } from 'react-native';
-// import Sound from 'react-native-sound';
 
+// import * as React from 'react';
+
+import { StyleSheet, Text, View, StatusBar, Image, ImageBackground, TouchableWithoutFeedback , Alert, Button } from 'react-native';
+// import Sound from 'react-native-sound';
+import { Audio } from 'expo-av';
 import button from './assets/button.png';
 import buttonPressed from './assets/button_pressed.png';
 
 
 
+
+
+
 export default class Bleep extends Component {
 
+    constructor() {
+      super();
+      this.state = { 
+        pressing: true
+      };
+    }
 
-        constructor() {
-          super();
-          this.state = { 
-            pressing: true
-          };
-        }
-        static navigationOptions = {
-          header: null,
-        };
-        // componentDidMount(){
-        //   const sound = new Sound('./assets/BLEEPSHORT.mp3', Sound.MAIN_BUNDLE, (error) => {
-        //     if (error) {
-        //       console.log('failed to load the sound', error);
-        //       return;
-        //     }
-        //   });
-        // }
-        renderImage()  {
-          var imgSource = this.state.pressing? button : buttonPressed;
-          return (
-            <Image
-            //   style={ homeStyles.optionsImage }
-              source={ imgSource }
-            />
-          );
-        }
-        playSound() {
-          if(this.state.pressing) {
-          alert("Bleeeeeep!!")}
-        }
+    static navigationOptions = {
+      header: null,
+    };
+
+    renderImage()  {
+      var imgSource = this.state.pressing? button : buttonPressed;
+      return (
+        <Image source={ imgSource }/> );
+    }
+
+//playing sound starts
+    async componentDidMount() {
+      Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+        playsInSilentModeIOS: true,
+        interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS,
+        shouldDuckAndroid: true,
+        staysActiveInBackground: true,
+        playThroughEarpieceAndroid: true
+      });
+
+      this.sound = new Audio.Sound()
+
+      const status = {
+        shouldPlay: false
+      };
+
+      this.sound.loadAsync(require('./assets/bleepshort.mp3'), status, false);
+    }
+   
+    playSound() {
+      // if(this.state.pressing) {alert("Bleeeeeep!!")};
+
+      this.sound.playAsync();
+      alert("Bleeeeeep!!")
+    };
+//playing sounds finish
+
+
 
     render() {
-        return (
-            <View style={styles.container}>
+      return (
+          <View style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor="#00000000" translucent={true}/>
-                <ImageBackground source={require('./assets/metal_background.jpg')} style={styles.backgroundimage}>
-                    <View style={styles.contents}>
-                        {/* <Text style={styles.text}>Hold to Bleep</Text> */}
-                        <TouchableWithoutFeedback  
-                        onPressIn={ () => this.setState({ pressing: !this.state.pressing }) } 
-                        onPressOut={ () => this.setState({ pressing: !this.state.pressing }) } >    
-                        {this.renderImage()}
-                        {/* <Image source={require('./assets/button.png')} ></Image> */}
-                        </TouchableWithoutFeedback >
-                    </View>
-                </ImageBackground>  
-            </View>
-        );
-        }
-        }
+            <ImageBackground source={require('./assets/metal_background.jpg')} style={styles.backgroundimage}>
+              <View style={styles.contents}>
+                  {/* <Text style={styles.text}>Hold to Bleep</Text> */}
+                  <TouchableWithoutFeedback  
+                    onPressIn={ () => this.setState({ pressing: !this.state.pressing }) } 
+                    onPressOut={ () => this.setState({ pressing: !this.state.pressing }) } >    
+                    {this.renderImage()}
+                  </TouchableWithoutFeedback >
+
+                  <Button
+                  title='Play Sound'
+                  color='#3CBBB1'
+                  onPress={this.playSound.bind(this)} />
+
+              </View>
+            </ImageBackground>  
+          </View>
+      );
+      }
+      }
 
 const styles = StyleSheet.create({
   container: {
